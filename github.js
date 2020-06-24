7c210d0f56f3627d6985470afc321f598d034258
DeltaForce14 <dita@ditapesek.co.uk>
2020-06-24 17:56:53 +0100

    first commit

---
+++ github.js
@@ -0,0 +1,44 @@
+// Fetch Data from GitHub
+// using async and await ES7
+
+class Github {
+    // this.repos_sort = 'created: asc'; used to sort repos from latest down
+    constructor() {
+      this.repos_count = 5;
+      this.repos_sort = 'created: asc';
+    }
+  
+    // get user method
+    async getUser(user) {
+
+   const profileResponse = await fetch(
+    `https://api.github.com/users/${user}`, 
+    {
+      headers: {
+        authorization: "token ##"
+      }
+    }
+  )
+  const repoResponse = await fetch(
+    `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`,
+    {
+      headers: {
+        authorization: "token ##"
+      }
+    }
+  )
+
+  const profileData = await profileResponse.json();
+  const repoData = await repoResponse.json();
+  
+      return {
+        profile: profileData,
+        repos: repoData,
+      }
+
+  }
+}
+
+
+
+//`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}
